89df0eeb949e6fcb6b6fe8d90da07fdd153d200d
ShuangYa <896640547@qq.com>
2017-08-03 21:33:25 +0800

    Update CodeMirror

--- third-party/codemirror/addon/lint/css-lint.js
+++ third-party/codemirror/addon/lint/css-lint.js
@@ -17,19 +17,12 @@
 
 CodeMirror.registerHelper("lint", "css", function(text) {
   var found = [];
-  if (!window.CSSLint) return found;
-
-  // STYLISH: hack start
-  var rules = CSSLint.getRules();
-  var allowedRules = ["display-property-grouping", "duplicate-properties", "empty-rules", "errors", "known-properties"];
-  CSSLint.clearRules();
-  rules.forEach(function(rule) {
-    if (allowedRules.indexOf(rule.id) >= 0) {
-      CSSLint.addRule(rule);
+  if (!window.CSSLint) {
+    if (window.console) {
+        window.console.error("Error: window.CSSLint not defined, CodeMirror CSS linting cannot run.");
     }
-  });
-  // STYLISH: hack end
-
+    return found;
+  }
   var results = CSSLint.verify(text), messages = results.messages, message = null;
   for ( var i = 0; i < messages.length; i++) {
     message = messages[i];
