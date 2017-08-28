bed7ae6503352d39875e52efea658f61680c98fd
ShuangYa <896640547@qq.com>
2017-08-28 21:11:08 +0800

    Add support for install .user.css file from other sites

--- scripts/inject/install.js
+++ scripts/inject/install.js
@@ -17,13 +17,57 @@ function sendEvent(type, data) {
 }
 
 function styleInstall () {
-	var styleName = getMeta('xstyle-name');
-	if (confirm(browser.i18n.getMessage('styleInstall', [styleName]))) {
-		getURL(getCodeUrl()).then((code) => {
-			styleInstallByCode(JSON.parse(code));
-		});
+	getURL(getCodeUrl()).then((code) => {
+		confirmAStyle(code);
+	});
+}
+
+function confirmAStyle(code) {
+	let json = null;
+	try {
+		json = JSON.parse(code);
+		if (Object.keys(json.advanced.item).length > 0) {
+			let saved = {};
+			for (let k in json.advanced.item) {
+				saved[k] = typeof(json.advanced.item[k].default) === 'undefined' ? Object.keys(json.advanced.item[k].option)[0] : json.advanced.item[k].default;
+			}
+			json.advanced.saved = saved;
+			json.sections = applyAdvanced(json.advanced.css, json.advanced.item, json.advanced.saved);
+		}
+	} catch (e) {
+		// is not a json file, try to parse as a .user.css file
+		if (trimNewLines(code).indexOf('/* ==UserStyle==') === 0) {
+			let meta = parseUCMeta(trimNewLines(code.match(/\/\* ==UserStyle==([\s\S]+)==\/UserStyle== \*\//)[1]));
+			let body = trimNewLines(code.replace(/\/\* ==UserStyle==([\s\S]+)==\/UserStyle== \*\//, ''));
+			json = {
+				"name": meta.name,
+				"updateUrl": meta.updateUrl || null,
+				"md5Url": meta.md5Url || null,
+				"url": meta.url || null,
+				"author": meta.author || null,
+				"originalMd5": meta.originalMd5 || null
+			};
+			if (Object.keys(meta.advanced).length > 0) {
+				let saved = {};
+				for (let k in meta.advanced) {
+					saved[k] = typeof(meta.advanced[k].default) === 'undefined' ? Object.keys(meta.advanced[k].option)[0] : meta.advanced[k].default;
+				}
+				json.advanced = {"item": meta.advanced, "saved": saved, "css": parseMozillaFormat(body)};
+				json.sections = applyAdvanced(json.advanced.css, json.advanced.item, json.advanced.saved);
+			} else {
+				json.advanced = {"item": {}, "saved": {}, "css": []};
+				json.sections = parseMozillaFormat(body);
+			}
+		} else {
+			alert(t('fileTypeUnknown'));
+			return;
+		}
+	}
+	if (confirm(browser.i18n.getMessage('styleInstall', [json.name]))) {
+		styleInstallByCode(json);
 	}
 }
+
 function styleInstallByCode(json) {
 	//Check whether the style has been installed
 	browser.runtime.sendMessage({method: "getStyles", url: json.url || getIdUrl() || location.href}).then((response) => {
@@ -37,17 +81,15 @@ function styleInstallByCode(json) {
 		});
 	});
 }
+
 document.addEventListener("xstyleInstall", styleInstall, false);
 
 // For open page
 if (window.location.href.indexOf('https://ext.firefoxcn.net/xstyle/install/open.html') === 0) {
-	var params = getParams();
+	let params = getParams();
 	if (params.code) {
 		getURL(params.code).then((code) => {
-			var json = JSON.parse(code);
-			if (confirm(browser.i18n.getMessage('styleInstall', [json.name]))) {
-				styleInstallByCode(json);
-			}
+			confirmAStyle(code);
 		});
 	}
 }
