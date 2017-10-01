f5d5520508921f1ace37e4cde6a5a1fd96ad4844
ShuangYa <896640547@qq.com>
2017-10-01 17:04:14 +0800

    Update translations

--- build/locales.php
+++ build/locales.php
@@ -1,7 +1,8 @@
 <?php
 require('config.php');
 define('OUTPUT_DIR', realpath(__DIR__ . '/../_locales') . '/');
-$language_list = ['en', 'zh_CN', 'zh_TW', 'sv_SE'];
+$default = 'en';
+$language_list = ['zh_CN', 'zh_TW', 'sv_SE'];
 $placeholders = json_decode(file_get_contents('locales_placeholder.json'), 1);
 function fetchUrl($url) {
 	$ch = curl_init($url);
@@ -14,6 +15,29 @@ function fetchUrl($url) {
 	@curl_close($ch);
 	return $r;
 }
+//Download default language
+if (!is_dir(OUTPUT_DIR . $default)) {
+	mkdir(OUTPUT_DIR . $default);
+}
+echo "Downloading $default ... ";
+$url = 'https://www.transifex.com/api/2/project/xstyle/resource/messages/translation/' . $default . '/';
+do {
+	$language = json_decode(fetchUrl($url), 1);
+} while (empty($language));
+$default_content = json_decode($language['content'], 1);
+ksort($default_content);
+file_put_contents('./output/message.json', str_replace('    ', "\t", json_encode($default_content, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)));
+// Add placeholders
+foreach ($placeholders as $kk => $vv) {
+	if (isset($default_content[$kk])) {
+		$default_content[$kk]['placeholders'] = $vv;
+	} else {
+		echo "\n$kk not exists, please check it\n";
+	}
+}
+file_put_contents(OUTPUT_DIR . $default . '/messages.json', json_encode($default_content, JSON_UNESCAPED_UNICODE));
+echo "ok\n";
+//Download other languages
 foreach ($language_list as $v) {
 	if (!is_dir(OUTPUT_DIR . $v)) {
 		mkdir(OUTPUT_DIR . $v);
@@ -25,15 +49,11 @@ foreach ($language_list as $v) {
 	} while (empty($language));
 	$content = json_decode($language['content'], 1);
 	ksort($content);
-	// remove description
-	if ($v !== 'en') {
-		foreach ($content as $kk => $vv) {
-			unset($content[$kk]['description']);
+	foreach ($content as $kk => $vv) {
+		unset($content[$kk]['description']); // remove description
+		if (empty($vv['message'])) {
+			$content[$kk]['message'] = $default_content[$kk]['message']; // set english words to it if it is empty
 		}
-	}
-	// put en to a special directory
-	if ($v === 'en') {
-		file_put_contents('./output/message.json', str_replace('    ', "\t", json_encode($content, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)));
 	}
 	// Add placeholders
 	foreach ($placeholders as $kk => $vv) {
