43fe327d55b79151bcf73608f26bc07fcdbdd705
ShuangYa <896640547@qq.com>
2017-11-18 14:37:31 +0800

    fixed build wrong manifest

--- build/build.js
+++ build/build.js
@@ -53,22 +53,28 @@ function deepCopy(obj) {
 	if (!obj || typeof obj != "object") {
 		return obj;
 	} else {
-		var emptyCopy = Object.create(Object.getPrototypeOf(obj));
+		var emptyCopy = obj instanceof Array ? [] : Object.create(Object.getPrototypeOf(obj));
 		return deepMerge(emptyCopy, obj);
 	}
 }
 function deepMerge(target, obj1 /* plus any number of object arguments */) {
 	for (var i = 1; i < arguments.length; i++) {
 		var obj = arguments[i];
-		for (var k in obj) {
-			// hasOwnProperty checking is not needed for our non-OOP stuff
-			var value = obj[k];
-			if (!value || typeof value != "object") {
-				target[k] = value;
-			} else if (k in target) {
-				deepMerge(target[k], value);
-			} else {
-				target[k] = deepCopy(value);
+		if (obj instanceof Array) {
+			for (var k in obj) {
+				target.push(obj[k]);
+			}
+		} else {
+			for (var k in obj) {
+				// hasOwnProperty checking is not needed for our non-OOP stuff
+				var value = obj[k];
+				if (!value || typeof value != "object") {
+					target[k] = value;
+				} else if (k in target) {
+					deepMerge(target[k], value);
+				} else {
+					target[k] = deepCopy(value);
+				}
 			}
 		}
 	}
