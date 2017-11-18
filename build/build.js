6fba9cbd31de9b3936f28486419f98edfefdb320
ShuangYa <896640547@qq.com>
2017-11-18 20:58:58 +0800

    Add transifex script

--- build/build.js
+++ build/build.js
@@ -4,6 +4,7 @@ const uglify = require('uglify-es');
 const cleancss = require('clean-css');
 const FirefoxExt = require('sign-addon').default;
 const crypto = require('crypto');
+const deepCopy = require('./deepCopy.js');
 
 const config = require('./config.json');
 const CleanCSSOptions = require('./clean_css_config.json');
@@ -48,37 +49,6 @@ function createCrx(fileContent, publicKey) {
 		fileContent.copy(crx, 16 + keyLength + sigLength);
 		resolve(crx);
 	});
-}
-function deepCopy(obj) {
-	if (!obj || typeof obj != "object") {
-		return obj;
-	} else {
-		var emptyCopy = obj instanceof Array ? [] : Object.create(Object.getPrototypeOf(obj));
-		return deepMerge(emptyCopy, obj);
-	}
-}
-function deepMerge(target, obj1 /* plus any number of object arguments */) {
-	for (var i = 1; i < arguments.length; i++) {
-		var obj = arguments[i];
-		if (obj instanceof Array) {
-			for (var k in obj) {
-				target.push(obj[k]);
-			}
-		} else {
-			for (var k in obj) {
-				// hasOwnProperty checking is not needed for our non-OOP stuff
-				var value = obj[k];
-				if (!value || typeof value != "object") {
-					target[k] = value;
-				} else if (k in target) {
-					deepMerge(target[k], value);
-				} else {
-					target[k] = deepCopy(value);
-				}
-			}
-		}
-	}
-	return target;
 }
 function readDir(dir) {
 	return new Promise((resolve) => {
