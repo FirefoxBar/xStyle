c50fd387e4eb45003d919c6296cac27e892a1c18
ShuangYa <896640547@qq.com>
2017-11-20 23:04:59 +0800

    Update stylelint rules

--- third-party/stylelint/codemirror.js
+++ third-party/stylelint/codemirror.js
@@ -9,36 +9,32 @@
 "use strict";
 
 let config = (defaultSeverity => ({
-  // 'sugarss' is a indent-based syntax like Sass or Stylus
-  // ref: https://github.com/postcss/postcss#syntaxes
-  syntax: 'sugarss',
-  // ** recommended rules **
-  // ref: https://github.com/stylelint/stylelint-config-recommended/blob/master/index.js
-  rules: {
-    'at-rule-no-unknown': [true, defaultSeverity],
-    'block-no-empty': [true, defaultSeverity],
-    'color-no-invalid-hex': [true, defaultSeverity],
-    'declaration-block-no-duplicate-properties': [true, {
-      'ignore': ['consecutive-duplicates-with-different-values'],
-      'severity': 'warning'
-    }],
-    'declaration-block-no-shorthand-property-overrides': [true, defaultSeverity],
-    'font-family-no-duplicate-names': [true, defaultSeverity],
-    'function-calc-no-unspaced-operator': [true, defaultSeverity],
-    'function-linear-gradient-no-nonstandard-direction': [true, defaultSeverity],
-    'keyframe-declaration-no-important': [true, defaultSeverity],
-    'media-feature-name-no-unknown': [true, defaultSeverity],
-    /* recommended true */
-    'no-extra-semicolons': [true, defaultSeverity],
-    'no-invalid-double-slash-comments': [true, defaultSeverity],
-    'property-no-unknown': [true, defaultSeverity],
-    'selector-pseudo-class-no-unknown': [true, defaultSeverity],
-    'selector-pseudo-element-no-unknown': [true, defaultSeverity],
-    'selector-type-no-unknown': true, // for scss/less/stylus-lang
-    'string-no-newline': [true, defaultSeverity],
-    'unit-no-unknown': [true, defaultSeverity],
-
-  }
+	syntax: 'postcss-less',
+	// see https://stylelint.io/user-guide/rules/
+	rules: {
+		'at-rule-no-unknown': [true, defaultSeverity],
+		'block-no-empty': [true, defaultSeverity],
+		'color-no-invalid-hex': [true, defaultSeverity],
+		'declaration-block-no-duplicate-properties': [true, {
+			'ignore': ['consecutive-duplicates-with-different-values'],
+			'severity': 'warning'
+		}],
+		'declaration-block-no-shorthand-property-overrides': [true, defaultSeverity],
+		'font-family-no-duplicate-names': [true, defaultSeverity],
+		'function-calc-no-unspaced-operator': [true, defaultSeverity],
+		'function-linear-gradient-no-nonstandard-direction': [true, defaultSeverity],
+		'keyframe-declaration-no-important': [true, defaultSeverity],
+		'media-feature-name-no-unknown': [true, defaultSeverity],
+		'no-extra-semicolons': [true, defaultSeverity],
+		'no-invalid-double-slash-comments': [true, defaultSeverity],
+		'property-no-unknown': [true, defaultSeverity],
+		'selector-pseudo-class-no-unknown': [true, defaultSeverity],
+		'selector-pseudo-element-no-unknown': [true, defaultSeverity],
+		'selector-type-no-unknown': true, // for scss/less/stylus-lang
+		'shorthand-property-no-redundant-values': [true, defaultSeverity],
+		'string-no-newline': [true, defaultSeverity],
+		'unit-no-unknown': [true, defaultSeverity],
+	}
 }))({severity: 'warning'});
 
 CodeMirror.registerHelper('lint', 'stylelint', (code, callback) => {
